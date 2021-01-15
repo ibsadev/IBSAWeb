a40b321c039f75e703a1649ae62c9b4fd84fdbfc
Bryan Simca <bryansimca20@gmail.com>
2021-01-15 13:13:45 -0800

    Implemented Carousel on Home Page using test image

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react'
-
 import styled from 'styled-components'
 
-import Header from '../Header/Header'
+import BannerIMG from '../../images/ibsa_cover.jpg'
+
+import Carousel from 'react-bootstrap/Carousel'
 
 const BannerHeading = styled.h2`
    margin-top: 30px;
    font-size: 200px;
 `;
 
+const BannerContainer = styled.div`
+   position: relative;
+   height: 100vh;
+   padding-top: 50vh;
+   padding-bottom: 50vh;
+   background-image:  url("../../styles/css/images/overlay.png"), url(${props.imgURL});
+   background-position: center top;
+   background-size: cover;
+   line-height: 1.75;
+   text-align: center;
+`;
+
 export default class Banner extends Component {
    render() {
       return (
-         <div id="banner">
-				<BannerHeading>IBSA</BannerHeading>
-				<p>UCLA's  Bruins Student Association</p>
-			</div>
+         <Carousel>
+            <Carousel.Item>
+               <div id="banner">
+                  <BannerHeading>IBSA</BannerHeading>
+                  <p>UCLA's  Bruins Student Association</p>
+               </div>
+            </Carousel.Item>
+            <Carousel.Item>
+               <div id="banner">
+                  <BannerHeading>IBSA</BannerHeading>
+                  <p>UCLA's  Bruins Student Association</p>
+               </div>
+            </Carousel.Item>
+            <Carousel.Item>
+               <div id="banner">
+                  <BannerHeading>IBSA</BannerHeading>
+                  <p>UCLA's  Bruins Student Association</p>
+               </div>
+            </Carousel.Item>
+         </Carousel>
       )
    }
 }
--- frontend/src/components/utils/carousel/ImageCarousel.js
+++ frontend/src/components/utils/carousel/ImageCarousel.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import styled from 'styled-components'
+
+const ImageContainer = styled.div`
+   position: relative;
+   height: 100vh;
+   padding-top: 50vh;
+   padding-bottom: 50vh;
+   background-image:  url("../../styles/css/images/overlay.png"), url("../../images/ibsa_cover.jpg");
+   background-position: center top;
+   background-size: cover;
+   line-height: 1.75;
+   text-align: center;
+`;
 
 /**
  * Takes in an array of 
@@ -7,6 +20,7 @@ import Carousel from 'react-bootstrap/Carousel'
 export default function ImageCarousel (props) {
    return (
       <Carousel>
+
       </Carousel>
    )
 }
