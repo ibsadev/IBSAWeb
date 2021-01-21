3756c27846480754757a116b2181c6f9795dd9e3
Bryan Simca <bryansimca20@gmail.com>
2021-01-21 15:08:35 -0800

    Added Home Carousel banner component

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,50 +1,27 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
-import BannerIMG from '../../images/ibsa_cover.jpg'
-
-import Carousel from 'react-bootstrap/Carousel'
+import HomeCarousel from '../utils/HomeCarousel'
+import Banner1 from '../../images/ibsa_cover.jpg'
+import Banner2 from '../../images/ibsa_cover2.JPG'
 
 const BannerHeading = styled.h2`
    margin-top: 30px;
    font-size: 200px;
 `;
 
-const BannerContainer = styled.div`
-   position: relative;
-   height: 100vh;
-   padding-top: 50vh;
-   padding-bottom: 50vh;
-   background-image:  url("../../styles/css/images/overlay.png"), url(${props.imgURL});
-   background-position: center top;
-   background-size: cover;
-   line-height: 1.75;
-   text-align: center;
-`;
+const carousel_content = [
+   {
+      image:Banner1
+   },
+   {
+      image:Banner2
+   }
+]
 
 export default class Banner extends Component {
    render() {
       return (
-         <Carousel>
-            <Carousel.Item>
-               <div id="banner">
-                  <BannerHeading>IBSA</BannerHeading>
-                  <p>UCLA's  Bruins Student Association</p>
-               </div>
-            </Carousel.Item>
-            <Carousel.Item>
-               <div id="banner">
-                  <BannerHeading>IBSA</BannerHeading>
-                  <p>UCLA's  Bruins Student Association</p>
-               </div>
-            </Carousel.Item>
-            <Carousel.Item>
-               <div id="banner">
-                  <BannerHeading>IBSA</BannerHeading>
-                  <p>UCLA's  Bruins Student Association</p>
-               </div>
-            </Carousel.Item>
-         </Carousel>
+         <HomeCarousel contents={carousel_content}/>
       )
    }
 }
