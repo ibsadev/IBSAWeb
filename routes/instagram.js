22ab786b8aec8bf567eb0115b1c9bd461ffb1d3c
Bryan Simca <bryansimca20@gmail.com>
2021-02-19 14:58:09 -0800

    Added route to obtain data from database for past and upcoming events, tested through postman

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const UpcomingEvent = require('../models/UpcomingEvent');
+const PastEvent = require('../models/PastEvent');
 
 require('dotenv').config();
 
@@ -15,8 +17,33 @@ router.get('/homepage', async(req, res, next) => {
 
 router.get('/pastevents', async(req, res, next) => {
    try {
-      let results = await axios.get(`https://graph.instagram.com/me/media?fields=id,media_type,permalink,media_url&access_token=${process.env.IG_ACC_TOKEN_ADMIN}`)
-      res.status(200).send(results.data)
+      PastEvent.find({}, (err, items) => {
+         if (err) {
+            res.status(500).send({
+               message: "Failed to reach server, please try again later"
+            })
+            return;
+         } 
+         res.status(200).send(items);
+      })
+   } catch {
+      return next(err)
+   }
+})
+
+router.get('/upcomingevents', async(req, res, next) => {
+   try {
+      UpcomingEvent.find({}, (err, items) => {
+         if (err) {
+            res.status(500).send({
+               message: "Failed to reach server, please try again"
+            })
+            return;
+         }
+         currentTimeInMillisecond = Date.now();
+         filteredData = items.filter(item => Date.parse(item.endTime) > currentTimeInMillisecond)
+         res.status(200).send(items)
+      })
    } catch {
       return next(err)
    }
