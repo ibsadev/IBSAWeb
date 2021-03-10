c22aec8dc5ac84326401cdbd1d28ce78cd2a8f0c
ntjoar <nathan.tjoar@gmail.com>
2021-03-10 15:12:32 -0800

    Bad URI for endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export default {
   // apiBaseUrl: 'http://localhost:8000/api'
-  apiBaseUrl: 'https://ibsa-display-sub.herokuapp.com//api',
+  apiBaseUrl: 'https://ibsa-display-sub.herokuapp.com/api',
 };
